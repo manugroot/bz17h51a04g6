daa6f6e4f88ea0767abf8b7e11e84afd1a7448f4
manugroot <manishreddy9963@gmail.com>
2020-01-08 10:31:33 +0530

    2 commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 //server static files
-app.use(express.static(__dirname + '/dist/ngproject'));
+app.use(express.static(__dirname + '/dist/ngproj'));
 
 //send index.html from home(root) route
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproj/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
